de487c64884e78ce40ebf4229d92c82f9e9b0d48
Ahmadelgndy <74977392+a7madelgndy@users.noreply.github.com>
2024-12-30 01:57:47 +0200

    Initial Commit

---
+++ Tu reminder/Tu_reminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tu_reminderApp.swift
+//  Tu reminder
+//
+//  Created by Ahmed El Gndy on 30/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Tu_reminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
